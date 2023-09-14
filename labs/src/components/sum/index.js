924bb68be7b8833ab421947ff83473af6d6d3f68
unknown <nguyenxuanuyen.94@gmail.com>
2023-09-14 20:14:52 +0700

    d

--- labs/src/components/sum/index.js
+++ labs/src/components/sum/index.js
@@ -2,21 +2,31 @@ import {useState} from 'react';
 
 function Sum(){
     const [soA, setSoA] = useState(0)
+    const [soB, setSoB] = useState(0)
+    const [totalAB, setTotal] = useState(0)
+    console.log('So A =', soA)
 
     function handleAChange(e){
         setSoA(e.target.value)
         
     }
-    
-    console.log('So A =', soA)
 
+    function handleBChange(e){
+        setSoB(e.target.value)
+        
+    }  
+
+    function total(e){
+        setTotal(Number(soA)+ Number(soB))
+    }
+    
     return(
         <div>
             <p>Tổng 2 số nguyên</p>
             <input type='number' onChange={handleAChange}/><br/>
-            <input type='number'/><br/>
-            <button>Tính tổng</button>
-            
+            <input type='number' onChange={handleBChange}/><br/>
+            <button onClick={total}>Tính tổng</button>
+            <p>{soA} + {soB} = {totalAB}</p>
         </div>
     )
 }
